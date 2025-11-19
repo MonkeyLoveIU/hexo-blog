6cb2a2d43c9a9b9cd08a3de321e63a47786494d7
124578h <113892611+124578h@users.noreply.github.com>
2025-11-19 22:04:14 +0800

    Site updated: 2025-11-19 22:04:13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1763560747849";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1763561043594";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
